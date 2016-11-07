5e9e16486dd17f44b933ddac8632137de5ff45fa
Justin Barabander <jbarabander@gmail.com>
2016-11-07 11:42:23 -0500

    Set up basic player test

---
+++ app/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+app.use(express.static('views'));
+app.use(express.static('node_modules'));
+
+router.get('/', function () {
+  res.send('index.html')
+});
+
+app.use('/', router);
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
