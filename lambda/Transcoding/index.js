8f68c6bc657c515abd22ab3b76991f42cf129dc3
Justin Barabander <jbarabander@gmail.com>
2016-11-08 10:19:05 -0500

    Modified transcoding to use true adaptation and nodified video sizing

--- lambda/Transcoding/index.js
+++ lambda/Transcoding/index.js
@@ -13,7 +13,10 @@ var et = new AWS.ElasticTranscoder({
 
 var validVideoFormats = [
   'mp4',
-  'flv'
+  'flv',
+  'ogg',
+  'avi',
+  'mov'
 ];
 
 exports.handler = function (event, context) {
@@ -33,6 +36,63 @@ exports.handler = function (event, context) {
     return;
   }
 
+  var hls64kAudioPresetId = '1351620000001-200071';
+  var hls0400kPresetId = '1351620000001-200050';
+  var hls0600kPresetId = '1351620000001-200040';
+  var hls1000kPresetId = '1351620000001-200030';
+  var hls1500kPresetId = '1351620000001-200020';
+  var hls2000kPresetId = '1351620000001-200010';
+  var segmentDuration = '10';
+
+  var hls64kAudio = {
+    Key: 'hls64kAudio/' + newKey,
+    PresetId: hls64kAudioPresetId,
+    SegmentDuration: segmentDuration
+  };
+  var hls0400k = {
+    Key: 'hls0400k/' + newKey,
+    PresetId: hls0400kPresetId,
+    SegmentDuration: segmentDuration
+  };
+
+  var hls0600k = {
+    Key: 'hls0600k/' + newKey,
+    PresetId: hls0600kPresetId,
+    SegmentDuration: segmentDuration
+  };
+
+  var hls1000k = {
+    Key: 'hls1000k/' + newKey,
+    PresetId: hls1000kPresetId,
+    SegmentDuration: segmentDuration
+  };
+
+  var hls1500k = {
+    Key: 'hls1500k/' + newKey,
+    PresetId: hls1500kPresetId,
+    SegmentDuration: segmentDuration
+  };
+
+  var hls2000k = {
+    Key: 'hls2000k/' + newKey,
+    PresetId: hls2000kPresetId,
+    SegmentDuration: segmentDuration,
+    ThumbnailPattern: 'thumbs-' + newKey + '-{count}'
+  };
+
+  var outputs = [
+    hls64kAudio,
+    hls0400k,
+    hls0600k,
+    hls1000k,
+    hls1500k,
+    hls2000k
+  ];
+
+  var outputKeys = outputs.map(function (element) {
+    return element.Key;
+  });
+
   var params = {
     PipelineId: pipelineId,
     OutputKeyPrefix: newKey + '/',
@@ -44,10 +104,11 @@ exports.handler = function (event, context) {
       Interlaced: 'auto',
       Container: 'auto'
     },
-    Outputs: [{
-      Key: 'hls-' + newKey,
-      ThumbnailPattern: 'thumbs-' + newKey + '-{count}',
-      PresetId: '1351620000001-200010'
+    Outputs: outputs,
+    Playlists: [{
+        Name : 'hls_' + newKey,
+        Format : 'HLSv3',
+        OutputKeys: outputKeys
     }]
   };
   et.createJob(params, function (err, data) {
